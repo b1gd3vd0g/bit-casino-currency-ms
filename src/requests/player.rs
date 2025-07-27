e28f4bd0b99fc1fbac1199a7211d46d353b15a80
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-27 14:39:42 -0700

    export the authenticate_player_token function which calls the player microservice in order to authenticate a player token and find its id.

---
+++ src/requests/player.rs
@@ -0,0 +1,41 @@
+use reqwest::{Client, StatusCode, header::HeaderMap};
+use uuid::Uuid;
+
+use crate::{handlers::responses::MessageResponse, requests::structs::PlayerId};
+
+/// Make a request to the player microservice attempting to validate a player authentication token.
+///
+/// **Request made:** `GET <player-ms>/authn`
+///
+/// # Arguments
+/// * token - The player's authentication JWT.
+///
+/// # Returns
+/// * The player_id of the authenticated player on success; otherwise, a MessageResponse object
+///   explaining what went wrong.
+pub async fn authenticate_player_token(token: String) -> Result<Uuid, MessageResponse> {
+    let client = Client::new();
+
+    let mut hm = HeaderMap::new();
+    let hv = format!("Bearer {}", token);
+    hm.insert("Authorization", hv.parse().unwrap());
+
+    let response = client
+        .get("http://localhost:3000/authn")
+        .headers(hm)
+        .send()
+        .await;
+
+    let response = match response {
+        Ok(r) => r,
+        Err(_) => return Err(MessageResponse::new("Token authentication request failed")),
+    };
+
+    match response.status() {
+        StatusCode::OK => {
+            let player: PlayerId = response.json().await.unwrap();
+            Ok(player.id)
+        }
+        _ => return Err(MessageResponse::token_auth_failure()),
+    }
+}
