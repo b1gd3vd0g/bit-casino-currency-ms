f36774a22e103627ced088e801b5aa55febf0cc3
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-29 13:22:56 -0700

    add a function to handle a transaction in a bit wallet.

--- src/handlers/transaction.rs
+++ src/handlers/transaction.rs
@@ -1,1 +1,88 @@
+use axum::{
+    Json,
+    extract::State,
+    http::HeaderMap,
+    response::{IntoResponse, Response},
+};
+use bigdecimal::BigDecimal;
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
 
+use crate::{
+    db::queries::{commit_transaction, update_wallet_balance},
+    handlers::{helper::extract_authn_token, responses::MessageResponse},
+    requests::player::authenticate_player_token,
+};
+
+#[derive(Deserialize)]
+pub struct RequestBody {
+    pub amount: BigDecimal,
+    pub reason: String,
+}
+
+#[derive(Serialize)]
+pub struct BalanceResponse {
+    pub balance: BigDecimal,
+}
+
+pub async fn handle_transaction(
+    State(pool): State<PgPool>,
+    headers: HeaderMap,
+    Json(body): Json<RequestBody>,
+) -> Response {
+    let auth_failure_response = (
+        StatusCode::UNAUTHORIZED,
+        Json(MessageResponse::token_auth_failure()),
+    )
+        .into_response();
+
+    let auth_token = match extract_authn_token(headers) {
+        Ok(t) => t,
+        Err(_) => return auth_failure_response,
+    };
+
+    let id = match authenticate_player_token(auth_token).await {
+        Ok(v) => v,
+        Err(response) => return (StatusCode::UNAUTHORIZED, Json(response)).into_response(),
+    };
+
+    let amount = BigDecimal::from(body.amount);
+
+    let updated_wallet = update_wallet_balance(&pool, id, amount.clone()).await;
+
+    let log_transaction = match updated_wallet {
+        Ok(_) => commit_transaction(&pool, id, amount, body.reason, None).await,
+        Err(_) => {
+            let message = String::from("Failed to update the balance.");
+            commit_transaction(&pool, id, amount, body.reason, Some(message)).await
+        }
+    };
+
+    match (updated_wallet, log_transaction) {
+        (Ok(w), Ok(_)) => {
+            (StatusCode::OK, Json(BalanceResponse { balance: w.balance })).into_response()
+        }
+        (Err(_), Ok(_)) => (
+            StatusCode::CONFLICT,
+            Json(MessageResponse::new(
+                "Could not complete the transaction. Likely a balance failure.",
+            )),
+        )
+            .into_response(),
+        (Ok(_), Err(_)) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(MessageResponse::new(
+                "Transaction completed but not logged! Unlikely and bad!",
+            )),
+        )
+            .into_response(),
+        (Err(_), Err(_)) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(MessageResponse::new(
+                "Transaction not completed or logged. Likely a database issue.",
+            )),
+        )
+            .into_response(),
+    }
+}
